e605d6cc4d0a86ac15c788e771da44eed1b6c261
nicolascampbell <nicodsm7@gmail.com>
2021-05-09 10:05:22 +0200

    Created and used preset for login form

---
+++ vue/src/components/login_form.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-form>
+      <b-form-group
+          id="input-group-1"
+          label="Email address:"
+          label-for="input-1"
+          description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            placeholder="Enter email"
+            required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+            id="input-2"
+            v-model="form.name"
+            placeholder="Enter username"
+            required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'login_form',
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+      },
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style>
+
+</style>
