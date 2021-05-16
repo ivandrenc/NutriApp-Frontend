67f16a14b3f99a3fddbb75afbfc2b91fdc24811a
nicolascampbell <nicodsm7@gmail.com>
2021-05-16 15:22:55 +0200

    Started working on validation for login

--- vue/src/components/login_form.vue
+++ vue/src/components/login_form.vue
@@ -1,32 +1,38 @@
 <template>
-  <div>
+  <div class="container">
     <b-form>
       <b-form-group
           id="input-group-1"
           label="Email address:"
           label-for="input-1"
-          description="We'll never share your email with anyone else."
       >
         <b-form-input
             id="input-1"
             v-model="form.email"
             type="email"
-            placeholder="Enter email"
+            placeholder="Enter it here"
+            :validation="validateEmail"
             required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+      <b-form-group
+          id="input-group-2"
+          label="Password:"
+          label-for="input-2"
+      >
         <b-form-input
             id="input-2"
-            v-model="form.name"
-            placeholder="Enter username"
+            v-model="form.password"
+            type="password"
+            placeholder="Enter your password"
+            :validation="validatePassword"
             required
         ></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button @click="validate_login" type="submit" variant="primary">Login</b-button>
+      <b-button type="reset" variant="danger">Register</b-button>
     </b-form>
 
   </div>
@@ -39,15 +45,44 @@ export default {
     return {
       form: {
         email: '',
-        name: '',
+        password: '',
       },
+      users: [
+        {
+          username: "roberto@outlook.com",
+          password: "123"
+        },
+        {
+          username: "Juanito@gmail.com",
+          password: "456"
+        }
+      ],
+      reg: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
     }
   },
   methods: {
+    validateEmail: function(){
+      return (this.email == "")? "" : (this.reg.test(this.email)) ? true : false;
+  },
+  computed: {
+      },
+      validatePassword: function(){
+        return 0;
+      }
+    }
 
-  }
+
 }
 </script>
 <style>
 
+body {
+  text-align: center;
+  padding: 50px;
+}
+.container {
+  padding: 40px 80px;
+  background-color: #fff;
+  border-radius: 8px;
+}
 </style>
