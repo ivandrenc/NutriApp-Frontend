a13b0a8a39eb2eb2f3688ffe8995825839046e39
Ivan Naranjo <ivandrenc@gmail.com>
2021-08-26 09:21:24 -0500

    register view and new components dashboard

--- vue/src/components/login_form.vue
+++ vue/src/components/login_form.vue
@@ -4,58 +4,54 @@
       <b-col cols="6" offset="3">
         <b-card title="Login" bg-variant="light">
           <b-card-body>
-             <b-form-group
-              id="input-group-0"
-              label="Username"
-              label-for="input-0"
-            >
+
+            <b-form-group id="username-login" label-for="input-0">
               <!--:validation has to ask server if that username is taken-->
               <b-form-input
-                id="input-0"
-                v-model="form.username"
-                type="text"
-                placeholder="Cool username"
-                required
+                  id="input-0"
+                  v-model="form.username"
+                  type="text"
+                  placeholder="Username"
+                  required
               ></b-form-input>
             </b-form-group>
 
-             <b-form-group
-              id="input-group-2"
-              label="Password"
-              label-for="input-2"
-            >
+            <b-form-group id="password-login" label-for="input-2">
               <!--:validation has to actually be named state as above,-->
               <!-- password validation here to avoid unnecessary requests to server-->
               <b-form-input
-                id="input-2"
-                v-model="form.password"
-                type="password"
-                placeholder="Password"
-                :state="validatePassword"
-                required
+                  id="input-2"
+                  v-model="form.password"
+                  type="password"
+                  placeholder="Password"
+                  :state="validatePassword"
+                  required
               ></b-form-input>
             </b-form-group>
 
             <div class="d-flex flex-row-reverse">
               <b-link style="color: rgb(61, 61, 61); font-size: 0.9em;"
-                >Forgot Password?</b-link
+              >Forgot Password?
+              </b-link
               >
             </div>
+
           </b-card-body>
           <div class="d-flex justify-content-center">
             <b-button @click="validateLogin" type="submit" variant="dark"
-              >Login
+            >Login
             </b-button>
-             <p v-if="msg">{{ msg }}</p>
+            <p v-if="msg">{{ msg }}</p>
           </div>
         </b-card>
         <div class="d-flex justify-content-center">
-            Dont have an account yet?
+          Dont have an account yet?
         </div>
         <div class="d-flex justify-content-center">
-            <b-link href="/register"
-                >Register!</b-link
-              >
+          <b-link href="/register"
+          >Register!
+          </b-link
+          >
         </div>
 
       </b-col>
@@ -64,7 +60,6 @@
 </template>
 
 <script>
-import AuthService from '@/services/AuthService.js';
 
 export default {
   name: "login_form",
@@ -77,27 +72,24 @@ export default {
       msg: "",
     }
   },
-  methods:{
+  methods: {
     async validateLogin() {
-      try {
-        const credentials = {
-          username: this.username,
-          password: this.password
-        };
-        const response = await AuthService.login(credentials);
-        
-        this.msg = response.msg;
+      //  const credentials = {
+      //    username: this.username,
+      //    password: this.password
+      //  };
+     //   const response = await AuthService.login(credentials);
 
-        const token = response.token;
+     //   this.msg = response.msg;
 
-        const user = response.user;
+        //const token = response.token;
 
-        this.$store.dispatch('login', { token, user });
-        this.$router.push('/');
+       // const user = response.user;
 
-      } catch (error) {
-        this.msg = error.response.data.msg;
-      }
+       // this.$store.dispatch('login', {token, user});
+        this.$router.push('/dashboard');
+
+
     },
   }
 }
---
+++ vue/src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <register-form></register-form>
+</template>
+
+<script>
+import register_form from "@/components/register_form";
+export default {
+  name:"Register",
+  components: {
+    registerForm: register_form,
+  }
+}
+</script>
+
+<style>
+
+</style>
